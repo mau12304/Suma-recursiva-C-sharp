4daaf796dcdd86944ba1cc243c269c9488b678a4
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-29 09:58:21 -0600

    Agregar archivos de proyecto.

---
+++ Suma Recursiva C#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static int SumaRecursiva(int n)
+    {
+        if (n <= 9)
+            return n;
+        else
+            return SumaRecursiva(n / 10) + n % 10;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresa un número entero: ");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            int resultado = SumaRecursiva(n);
+            Console.WriteLine($"La suma de los dígitos de {n} es: {resultado}");
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Debes ingresar un número entero.");
+        }
+    }
+}
